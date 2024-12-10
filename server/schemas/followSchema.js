686547f0e3ac85635d0c780d251b9c37dec61158
FauzhanWahyudi <89763959+FauzhanWahyudi@users.noreply.github.com>
2024-12-10 20:15:17 +0700

    feat: add validation to add following

--- server/schemas/followSchema.js
+++ server/schemas/followSchema.js
@@ -1,5 +1,7 @@
+const { ObjectId } = require("mongodb");
 const { db } = require("../config/db");
 const Follow = require("../models/follow");
+const User = require("../models/user");
 
 const followTypeDefs = `#graphql
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
@@ -34,13 +36,37 @@ const followResolvers = {
   Mutation: {
     addFollowing: async (parent, args, contextValue) => {
       const { user } = await contextValue.auth();
-      args.body.followerId = user._id;
+
+      //check input
       if (!args.body.followingId) {
         throw new Error("FollowingId is required");
       }
-      if (!args.body.followerId) {
-        throw new Error("FollowerId is required");
+
+      //convert following id to ObjectId
+      args.body.followingId = new ObjectId(args.body.followingId);
+
+      //check if user id same as followingId
+      if (args.body.followingId.toString() === user._id.toString()) {
+        throw new Error("You can't follow yourself");
       }
+
+      //check if user with _id = following id is found
+      const followingUser = await User.collection.findOne({
+        _id: args.body.followingId,
+      });
+      if (!followingUser) throw new Error("User not found");
+
+      //check if user already follow
+      const follow = await Follow.collection.findOne({
+        followerId: user._id,
+        followingId: args.body.followingId,
+      });
+      if (follow) throw new Error("You already follow user");
+
+      //assign user id to be body.follower id
+      args.body.followerId = user._id;
+
+      //create new follow and return new follow
       return await Follow.addFollowing(args.body);
     },
   },
