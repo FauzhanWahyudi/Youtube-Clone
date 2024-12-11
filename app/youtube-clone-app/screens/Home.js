a1056541aa84376559b8fed8783b7e97737d4bc0
FauzhanWahyudi <89763959+FauzhanWahyudi@users.noreply.github.com>
2024-12-11 15:59:49 +0700

    fix: unnecessary string inside login screen and add some comments

--- app/youtube-clone-app/screens/Home.js
+++ app/youtube-clone-app/screens/Home.js
@@ -1,12 +1,12 @@
 import { TouchableHighlight, View, Text } from "react-native";
 
 export default function Home({ navigation }) {
-  // const navigation = useNavigation(); ini bisa dipake
-  // pas butuh yang udh dalem components nya
+  // const navigation = useNavigation();
+  //can be used when component is 2 level lower from Navigation (Stack or etc) like card inside Home
   return (
-    <View className="">
+    <View className="flex-1 justify-center items-center p-2">
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>Youtube Haward</Text>
+      <Text>Youtube</Text>
       <TouchableHighlight onPress={() => navigation.navigate("Login")}>
         <View className="bg-slate-600 p-2">
           <Text>Login</Text>
--- app/youtube-clone-app/screens/Login.js
+++ app/youtube-clone-app/screens/Login.js
@@ -2,8 +2,8 @@ import { Text, TouchableHighlight, View } from "react-native";
 
 export default function Login({ navigation }) {
   return (
-    <View>
-      <Text>Login</Text>;
+    <View className="flex-1 justify-center items-center p-2">
+      <Text>Login</Text>
       <TouchableHighlight onPress={() => navigation.navigate("Home")}>
         <View className="bg-slate-600 p-2">
           <Text>Home</Text>
