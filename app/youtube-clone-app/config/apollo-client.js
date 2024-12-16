905827db250eb86c0d98dec87e642da26c723a77
FauzhanWahyudi <fauzhanwahyudi@gmail.com>
2024-12-16 10:50:25 +0700

    feat: change apollo server link

--- app/youtube-clone-app/config/apollo-client.js
+++ app/youtube-clone-app/config/apollo-client.js
@@ -3,7 +3,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "https://4d4d-139-228-111-122.ngrok-free.app/",
+  uri: "https://youtube-clone-server.fauzhanwahyudi.com/",
 });
 
 const authLink = setContext((_, { headers }) => {
